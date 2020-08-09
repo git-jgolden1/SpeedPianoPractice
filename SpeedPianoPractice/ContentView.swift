91c3b92dc868caef56aeb0442237d021aa34ad7a
Jonathan Gurr <jonathan.golden.gurr@gmail.com>
2020-08-08 18:14:20 -0700

    Initial Commit

---
+++ SpeedPianoPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpeedPianoPractice
+//
+//  Created by Jonathan Gurr on 08-08-20.
+//  Copyright © 2020 Jonathan Gurr. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
